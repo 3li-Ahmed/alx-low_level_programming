ed4b4b7695c2e5e590cc6f4a70938b4d654072f0
3li-Ahmed <awdali259@gmail.com>
2023-10-27 14:05:56 -0700

    A

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,31 +1,19 @@
 #include <stdio.h>
-		
-#include "main.h"
-		
 
-		
+#include "main.h"
+
 /**
-		
  * main - prints the name of the program
-		
  * @argc: number of arguments
-		
  * @argv: array of arguments
-		
  *
-		
  * Return: Always 0 (Success)
-		
  */
-		
+
 int main(int argc __attribute__((unused)), char *argv[])
 {
-	// Add a blank line after the opening curly brace.
+    printf("%s\n", *argv);
 
-	printf("%s\n", *argv);
-
-	// Indent the printf() statement by one tab.
-
-	return (0);
+    return (0);
 }
 
