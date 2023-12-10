c6389d557bdeec5a1572dd1a822c2fbcd4ed52e7
3lawy <awdali259@gmail.com>
2023-12-10 13:15:24 -0800

    A

--- 2-main.c
+++ 2-main.c
@@ -1,21 +1,22 @@
-#include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
 
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
-int main(void)
+int main(int ac, char **av)
 {
-    char *s = "hello";
-    char *f;
+    int res;
 
-    f = _strchr(s, 'l');
-
-    if (f != NULL)
+    if (ac != 3)
     {
-        printf("%s\n", f);
+        dprintf(2, "Usage: %s filename text\n", av[0]);
+        exit(1);
     }
+    res = append_text_to_file(av[1], av[2]);
+    printf("-> %i)\n", res);
     return (0);
 }
